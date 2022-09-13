d07406bc18d35e665d34a848f0f19909b41a6849
Caio Cruz <covu@gft.com>
2022-09-13 17:37:31 -0300

    First commit meals app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'screens/categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Let\'s cook?',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
